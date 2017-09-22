1980ec87090e479f2a4883ae32b0aec9b54d507e
wangzheng <wangzheng@shimo.im>
2017-09-23 01:08:06 +0800

    feat: select file type

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,38 @@ const upload = multer({
     filename: (req, file, cb) => {
       // 可以参考 multer.diskStorage getFilename 源码
       crypto.pseudoRandomBytes(16, 
-        (err, row) => cb(err, err ? undefined : row.toString('hex') + `.${}`))
+        (err, row) => cb(err, err ? undefined : row.toString('hex')))
     }
   })
 })
 
+const types = [{
+  id: 0,
+  name: 'markdown'
+}, {
+  id: 1,
+  name: 'docx'
+}, {
+  id: 2,
+  name: 'epub'
+}, {
+  id: 3,
+  name: 'json'
+}, {
+  id: 4,
+  name: 'html'
+}]
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'parchment online'
+    title: 'parchment online',
+    types
   })
 })
 
 router.post('/file', upload.single('upload'), async (ctx, next) => {
+  // 上传的同时获取文件源类型,并与其真实类型作对比，不一致则抛出文件错误
+  // 转换时，以用户所选类型为准
+  console.log(ctx.req.body)
   ctx.body = {
     filename: ctx.req.file.originalname,
     mimetype: ctx.req.file.mimetype,
