7b3d4d0d26d4a3e581d83130a14eb8da783a41cb
wangzheng <wangzheng@shimo.im>
2017-09-23 00:53:03 +0800

    Try to modify the file name

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 const router = require('koa-router')()
 const multer = require('koa-multer')
-const upload = multer({ dest: 'uploads/' })
+var crypto = require('crypto')
+
+const upload = multer({ 
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => cb(null, 'uploads/'),
+    filename: (req, file, cb) => {
+      // 可以参考 multer.diskStorage getFilename 源码
+      crypto.pseudoRandomBytes(16, 
+        (err, row) => cb(err, err ? undefined : row.toString('hex') + `.${}`))
+    }
+  })
+})
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
@@ -15,5 +26,6 @@ router.post('/file', upload.single('upload'), async (ctx, next) => {
     fileCode: ctx.req.file.filename
   }
 })
+
 
 module.exports = router
